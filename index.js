b950371fa0ed2702a4afab24e9f054f6130c8400
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-05-11 21:44:34 +0600

    second commit

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true,
-    optionSuccessStatus: 200,
-}
+// const corsOptions = {
+//     origin: 'http://localhost:5173',
+//     credentials: true,
+//     optionSuccessStatus: 200,
+// }
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.bbzq5pl.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
@@ -31,6 +31,22 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
+
+        const serviceCollection = client.db('doctorConsultation').collection('service')
+
+        app.get('/service', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollection.findOne(query)
+            res.json(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
